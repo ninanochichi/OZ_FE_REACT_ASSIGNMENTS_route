1aefc883c7b3bc1685a29e9b1e7924419c2373b3
김우진 <kedw158@gmail.com>
2025-12-23 15:20:49 +0900

    React Router 과제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import "./App.css";
 import headerStyle from "./assets/styles/header.module.css";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import ErrorPage from "./pages/ErrorPage";
+
 /* Layout 컴포넌트
 1. /home으로 라우팅할 수 있는 <Link/>를 선언하세요.
 2. /about으로 라우팅할 수 있는 <Link/>를 선언하세요.
@@ -15,9 +22,14 @@ function Layout() {
         <div>Router Practice</div>
         <div className={headerStyle.headerLinks}>
           {/* 여기에 Link 컴포넌트를 할당하세요. */}
+          <Link to="/home">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to={`/random-${Math.random()}`}>Random</Link>
         </div>
       </header>
       {/* 이곳에 Outlet을 할당하세요. */}
+      <Outlet />
     </>
   );
 }
@@ -33,7 +45,19 @@ function Layout() {
 8. /* 모든 잘못된 라우팅에서 보이는 <ErrorPage/> 컴포넌트를 라우팅에 연결하세요.
 */
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
